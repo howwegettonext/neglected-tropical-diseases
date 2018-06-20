ff00680c7a9d487dabc6ea645008ed79717c4cda
edenbrack <edenbrack@gmail.com>
2018-06-20 09:38:49 -0400

    card size & format fixes

--- script/slider.js
+++ script/slider.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       $('.slider').slick({
       	centerMode: true,
       	dots: true,
-      	slidesToShow: 3,
+      	slidesToShow: 2.6,
       	centerPadding: '60px',
       	 responsive: [
 		    {
@@ -17,7 +17,17 @@ $(document).ready(function(){
 		      }
 		    },
 		    {
-		      breakpoint: 480,
+		      breakpoint: 720,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1.5,
+		        dots: false,
+		      }
+		    },
+		    {
+		      breakpoint: 550,
 		      settings: {
 		        arrows: false,
 		        centerMode: true,
@@ -25,7 +35,7 @@ $(document).ready(function(){
 		        slidesToShow: 1,
 		        dots: false,
 		      }
-		    }
+		     },
 		  ]
       });
     });
