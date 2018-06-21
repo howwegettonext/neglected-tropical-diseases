94a4ee61f8e0678a75cf8d369ea264dca6ff5c05
edenbrack <e.brackenbury1@uni.brighton.ac.uk>
2018-06-21 09:50:09 -0400

    arrows responsive fix

---
+++ slider.js
@@ -0,0 +1,40 @@
+//Initiialise slider
+
+$(document).ready(function(){
+      $('.slider').slick({
+      	centerMode: true,
+      	dots: true,
+      	slidesToShow: 2.6,
+      	centerPadding: '60px',
+      	 responsive: [
+		    {
+		      breakpoint: 950,
+		      settings: {
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 2,
+		      }
+		    },
+		    {
+		      breakpoint: 720,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1.5,
+		        dots: false,
+		      }
+		    },
+		    {
+		      breakpoint: 550,
+		      settings: {
+		        arrows: false,
+		        centerMode: true,
+		        centerPadding: '40px',
+		        slidesToShow: 1,
+		        dots: false,
+		      }
+		     },
+		  ]
+      });
+    });
